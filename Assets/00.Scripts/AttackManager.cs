5455506d8c7467b2be02a7d53a999f800ff87481
Lligar <lligar9497@gmail.com>
2022-02-11 01:17:29 +0900

    초기 세팅 및 공격 시스템 구현중
    배경, 플레이어 캐릭터, 적군, 스킬 사용할 버튼 구성

---
+++ Assets/00.Scripts/AttackManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackManager : MonoBehaviour
+{
+    public GameObject[] skills;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
